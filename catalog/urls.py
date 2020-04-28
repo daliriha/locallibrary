b2b62dbe02665b6f2107684ece73997b49806872
daliriha <daliri_ha@um.ac.ir>
2020-04-28 21:24:05 +0430

    Some models/views added.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
+app_name = "catalog"
 urlpatterns = [
+    url(r'^$', views.Index.as_view(), name="index"),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+
 
 ]
