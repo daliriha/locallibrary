88d71d2c68e09a1c6b5082348c833e5f89e902d8
daliriha <daliri_ha@um.ac.ir>
2020-04-29 21:16:03 +0430

    Update code.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,18 +1,21 @@
 from django.contrib import admin
 from . import models
 
+
 @admin.register(models.BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
-        ('Information', {
-            'fields': ('book', 'imprint', 'id')
+        (None, {
+            'fields': ('book','imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back','borrower')
         }),
     )
+
 
 
 class BooksInstanceInline(admin.TabularInline):
